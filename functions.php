ee7ef9fabc20345f65fa665f2f80a4bba1bd02b6
jayboyyy92 <jsvr159@gmail.com>
2025-05-28 09:46:24 +1000

    chore: remove test CSS and verify child theme scaffold

--- functions.php
+++ functions.php
@@ -1,2 +1,24 @@
 <?php
-// Child theme hooks and asset enqueues go here
+/**
+ * Enqueue Kadence parent + child styles properly.
+ */
+function grovespark_child_enqueue_styles() {
+  // 1) Enqueue the parent theme’s main stylesheet
+  wp_enqueue_style(
+    'kadence-parent-style',
+    get_template_directory_uri() . '/style.css'
+  );
+
+  // 2) Enqueue your child theme’s stylesheet, dependent on the parent 
+  wp_enqueue_style(
+    'grovespark-child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array( 'kadence-parent-style' ),
+    filemtime( get_stylesheet_directory() . '/style.css' )
+  );
+}
+add_action( 'wp_enqueue_scripts', 'grovespark_child_enqueue_styles' );
+
+
+  
+  
